de64e78bd4a8dabc9d76b303aa2729265f17b091
mahmoud000011 <mahmoud.kh.hassen@gmail.com>
2024-02-21 17:47:04 +0200

    the function is mine

---
+++ m.php/function.php
@@ -0,0 +1,95 @@
+<?php
+ /*
+echo "================================= ============ افهم ========================================";
+echo "<br>";
+function my_frinds( $elsaheb  ){
+    if ($elsaheb === "khaled"){
+        echo "yayaya my best frind";
+    }elseif($elsaheb==="joo"){
+        echo "gem man";
+    }elseif($elsaheb==="monir"){
+        echo "betaع farida";
+    }else{
+        echo "no item to me";
+    }
+}
+my_frinds("khaled");
+echo "<br>";
+my_frinds("joo");
+echo "<br>";
+my_frinds("monir");
+echo "<br>";
+my_frinds("asmat");
+echo "<br>";
+
+$addtion = 0;
+function sum ($a , $b ){
+    return $a + $b ;
+    ;
+}
+sum (2,5);
+var_dump ( sum (2,5));
+$mmm = sum (2,5);
+echo "<br >";
+echo $mmm; 
+echo "<br>";
+
+
+function test ($name = "mohamed" , $age = 20 ,$countrry = "priet"){
+    echo "my name is $name and old is $age and my country  is $countrry ";
+}
+test( ); 
+// is bulid in function ;
+echo "<br>";
+function  sumtion (){
+    echo  func_num_args () . "<br >";
+    echo func_get_arg(3) . "<br>";
+    echo "<pre>";
+    print_r(func_get_args());
+    echo "<pre>";
+    $result = 0;
+    foreach (func_get_args() as $arg){
+        $result +=$arg;
+    }
+    echo $result;
+
+} 
+ sumtion ( 5 ,5,22,44,88 ,33 );
+  // spalt oprratoir  ... $sss
+function khaled (...$num){
+    print_r(func_get_args());
+}
+khaled(1,2,3,45,6,7,8,9);
+
+function mahmoud (){
+    return "mahmoud khaled hassen  ";
+}
+$fun2 = "mahmoud";
+echo $fun2;
+echo "<br>";
+echo $fun2();
+echo "<br>";
+$mm ="mahmoud";
+echo strlen ($mm);
+echo "<br>";
+function node(&$num){
+     $test =$num+=100;
+    return $test ;
+}
+$mmm = 100;
+echo node($mmm);
+echo "<br>";
+echo $mmm;
+echo "<br>";
+
+function fade ($num1 , $nume2 ):int {
+    $sum = $num1 + $nume2 ;
+    return $sum ;
+}
+echo fade(5.5 , 6.5) ;
+echo "<br>";
+echo gettype( fade(5.5 , 6.5));
+
+for ($i=0 ; $i<=100 ; $i++){
+    echo "===========================================================   TEFA    ==========================================";
+*/
